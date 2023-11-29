7612f09521f53a4c30428593dc75778ea1b445ce
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-11-29 20:43:41 +0530

    used query selector

--- script.js
+++ script.js
@@ -9,13 +9,21 @@ addItem[0].style.color="green"
 addItem[0].style.fontWeight="bold"
 
 const listItems = document.getElementsByClassName("list-group-item")
-console.log(listItems)
 listItems[1].style.backgroundColor="green"
 
 for(var i=0;i<listItems.length;i++){
     listItems[i].style.fontWeight="bold"
 }
 
-const li = document.getElementsByTagName("li")
-li[4].style.fontWeight="bold"
-li[4].style.backgroundColor="red"
+// const li = document.getElementsByTagName("li")
+// li[4].style.fontWeight="bold"
+// li[4].style.backgroundColor="red"
+
+const li = document.querySelector(".list-group-item:nth-child(2)")
+li.style.backgroundColor="green"
+
+const li2 = document.querySelector(".list-group-item:nth-child(3)")
+li2.style.display="none"
+
+const li3 = document.querySelector("li:nth-child(odd)")
+li3.style.color="green"
