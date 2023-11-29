f12535f23562d84a761a84215d5cb932629cee6a
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-11-29 19:47:02 +0530

    added background attributes to list items

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ headertitle.innerHTML="new name"
 
 const addItem = document.getElementsByClassName("title");
 addItem[0].style.color="green"
-addItem.style.fontWeight="bold"
+addItem[0].style.fontWeight="bold"
+
+const listItems = document.getElementsByClassName("list-group-item")
+console.log(listItems)
+listItems[1].style.backgroundColor="green"
+
+for(var i=0;listItems.length;i++){
+    listItems[i].style.fontWeight="bold"
+}
