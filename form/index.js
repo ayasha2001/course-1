f9b7440eb957dd52a0f7c368aada5943b6f1ae92
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-11-30 20:43:02 +0530

    delete feature in appointment entry list

--- form/index.js
+++ form/index.js
@@ -1,8 +1,17 @@
+const ul = document.getElementById("container")
+ul.addEventListener("click",deleteEntry)
+
 function submitForm(event){
+
    const name = document.getElementById("name").value;
    const email = document.getElementById("email").value;
    const phone = document.getElementById("phone").value;
    const datetime = document.getElementById("datetime").value;
+
+   if(name=="" || email=="" || phone==""){
+      alert("enter all fields")
+      return;
+   }
 
    const user = {
      userName : name,
@@ -10,6 +19,34 @@ function submitForm(event){
      userPhone : phone,
      userDateTime:datetime
    }
-   
+
+   const li = document.createElement("li");
+   const btn = document.createElement("button");
+   const text = document.createTextNode(`${name} - ${email} - ${phone}`)
+   btn.innerHTML="delete"
+   btn.setAttribute("class","delete user")
+   li.setAttribute("class","li-tag");
+   li.appendChild(text)
+   li.appendChild(btn)
+
+   ul.appendChild(li)
+
+   document.getElementById("name").value="";
+   document.getElementById("email").value="";
+   document.getElementById("phone").value ="";
+   document.getElementById("datetime").value="";
+
    localStorage.setItem(email,JSON.stringify(user))
+}
+
+function deleteEntry(event){
+   event.preventDefault()
+   if(event.target.classList.contains("delete")!=-1){
+       const node = event.target.parentNode;
+       const key = event.target.parentNode.innerText.split("-")[1].trim();
+       localStorage.removeItem(key)
+       ul.removeChild(node)
+       console.log(key)
+       console.log(node)
+   }
 }
